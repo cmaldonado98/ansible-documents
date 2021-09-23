ff4c2a62374ba76c97b3bcba1207fc1f1909f327
Carlos Maldonado <carlos.maldonado1@cloud.uautonoma.cl>
2021-09-23 10:07:24 -0500

    First

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+  echo Script para crear archivos cache
+
+  echo Instalación de varios archivos
+
+  cd ../../
+
+  cd home
+
+  sudo touch cache1.sh
+  sudo touch cache2.sh
+  sudo touch cache3.sh
